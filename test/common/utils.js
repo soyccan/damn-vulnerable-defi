a669f76e7928aa346f2c336c54e18f36a7f16544
soyccan <soyccan@gmail.com>
2023-09-15 23:48:11 +0800

    User-defined artifact name
    Sometimes contracts involved in the transaction do not have their ABIs
    in project artifact dir. This is often the case for library contracts
    come from Node.js modules.
    
    In this case, you can specify the artifact name manually to make
    hardhat-tracer recognize them. The handy setTracerArtifactName() function
    will copy the artifact to the project artifact dir.
    
    Minor fixes: use the new arrow function syntax

--- test/common/utils.js
+++ test/common/utils.js
@@ -1,17 +1,73 @@
-const { tracer } = require("hardhat");
+const fs = require("node:fs");
+const fsPromises = require("node:fs/promises");
+const path = require("node:path");
 
-function setTracerTag(address, name) {
-    console.log(`${name}: ${address}`);
-    if (tracer) {
-        tracer.nameTags[address] = name;
-    }
+const { tracer } = require("hardhat");
+const { parseFullyQualifiedName } = require("hardhat/utils/contract-names");
+
+const setTracerTag = (address, name) => {
+    console.log(`${address}: ${name}`);
+    if (!tracer)
+        return;
+
+    tracer.nameTags[address] = name;
 }
 
-async function waitForTx(promiseOfTxResp) {
-    return await (await promiseOfTxResp).wait();
+const setTracerArtifactName = async (address, name) => {
+    console.log(`${address} => ${name}`);
+    if (!tracer)
+        return;
+
+    let { sourceName, contractName } = parseFullyQualifiedName(name);
+    setTracerTag(address, contractName);
+    tracer.artifactNames[address] = name;
+
+    // if artifact not exist, try to copy from node_modules
+    let artifactPath = hre.artifacts.formArtifactPathFromFullyQualifiedName(name);
+    if (fs.existsSync(artifactPath))
+        return;
+
+    // Example:
+    //   name: "@uniswap/v3-core/contracts/UniswapV3Factory.sol:UniswapV3Factory"
+    //   sourceName: "@uniswap/v3-core/contracts/UniswapV3Factory"
+    //   contractName: "UniswapV3Factory"
+    //   moduleName: "@uniswap/v3-core"
+    //   modulePath: "contracts/UniswapV3Factory.sol"
+    //   srcArtifactPath: "node_modules/@uniswap/v3-core/artifacts/contracts/UniswapV3Factory.sol/UniswapV3Factory.json"
+    //   artifactPath: "artifacts/@uniswap/v3-core/contracts/UniswapV3Factory.sol/UniswapV3Factory.json"
+    let sourceNameSplits = sourceName.split("/");
+
+    let moduleName = sourceNameSplits.shift();
+    if (moduleName.startsWith("@"))
+        moduleName = path.join(moduleName, sourceNameSplits.shift());
+
+    let modulePath = sourceNameSplits.join("/");
+    let srcArtifactPath = path.join(
+        process.cwd(),
+        "node_modules",
+        moduleName,
+        "artifacts",
+        modulePath,
+        `${contractName}.json`
+    );
+    await copyFileEnsureDir(srcArtifactPath, artifactPath);
+}
+
+const waitForTx = async promiseOfTxResp => await (await promiseOfTxResp).wait();
+
+const copyFileEnsureDir = async (src, dest) => {
+    try {
+        await fsPromises.mkdir(path.dirname(dest), { recursive: true });
+    } catch (err) {
+        if (err.code !== "EEXIST")
+            throw err;
+    }
+    await fsPromises.copyFile(src, dest);
 }
 
 module.exports = {
     setTracerTag,
+    setTracerArtifactName,
     waitForTx,
+    copyFileEnsureDir,
 };
